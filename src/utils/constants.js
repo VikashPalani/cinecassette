871bc5eb7b86805815d399bc007070d41f03a2b0
VikashPalani <vikash1702palani@gmail.com>
2024-07-08 11:21:46 +0530

    created .env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,12 +14,9 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNWMxNDg4N2QxYmNlYTAwZjkxNGU3YTdiYmYxY2ZhMSIsIm5iZiI6MTcyMDA2ODMyNS4zNTQ5NTEsInN1YiI6IjY2N2QwOGQwYTVjNDA3ZDMxMzExZWZkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vCqggvnfAVnJUFgGKVDvS0IqZ-vDfK_00TBBMC2-lDU'
+    Authorization: 'Bearer' + process.env.REACT_APP_TMDB_KEY ,
   }
 };
 
-// export const OPENAI_KEY = 'sk-proj-trprjFqF1MgDwToIgUO9T3BlbkFJuuV5z5OX1HnEFlo8p5sL';
 
 //Deployed URL: cine-cassette1.web.app
-//API KEY: 15c14887d1bcea00f914e7a7bbf1cfa1
-//API Read Access Token: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNWMxNDg4N2QxYmNlYTAwZjkxNGU3YTdiYmYxY2ZhMSIsIm5iZiI6MTcxOTk3Nzk0Ny4wMjI4NzEsInN1YiI6IjY2N2QwOGQwYTVjNDA3ZDMxMzExZWZkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nhJX02QHRnD_zLUIJPIS-KydegUyfQp4yF80BVEzWs4
